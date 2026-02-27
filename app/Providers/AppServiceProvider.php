9e19fc7c6ddc4d2ed7cd062ccca7524e2dcd02b1
Kirkyyx <kirktijol4@gmail.com>
2026-02-27 12:29:33 +0800

    ambot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,18 +2,20 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Auth\Events\Failed;
 use App\Listeners\LogSuccessfulLogin;
 use App\Listeners\LogFailedLogin;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function boot(): void
-    {
-        Event::listen(Login::class, LogSuccessfulLogin::class);
-        Event::listen(Failed::class, LogFailedLogin::class);
-    }
+    protected $listen = [
+        Login::class => [
+            LogSuccessfulLogin::class,
+        ],
+        Failed::class => [
+            LogFailedLogin::class,
+        ],
+    ];
 }
